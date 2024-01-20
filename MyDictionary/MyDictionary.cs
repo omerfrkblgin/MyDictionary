e52bb15ee0b617398b0a37dd205e81971358102d
Omer Faruk Bilgin <omerfrkblgin@gmail.com>
2024-01-20 15:49:41 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<T>
+    {
+        private T[] values;
+        public MyDictionary()
+        {
+            values = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = values;
+            values = new T[values.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                values[i] = tempArray[i];
+            }
+            values[values.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return values.Length; }
+
+        }
+        public T[] Items
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> books = new MyDictionary<string>();
+
+            books.Add("Tekvin");
+            books.Add("Patasana");
+            books.Add("Kaşağı");
+            books.Add("Sefiller");
+            Console.WriteLine("Kitaplar");
+            Console.WriteLine("-----------------");
+
+            foreach (var book in books.Items)
+            {
+                
+                Console.WriteLine(book);
+            }
+            
+            MyDictionary<int> pages = new MyDictionary<int>();
+
+            pages.Add(350);
+            pages.Add(268);
+            pages.Add(154);
+            pages.Add(745);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Sayfaları");
+            Console.WriteLine("-----------------");
+
+            foreach (var page in pages.Items)
+            {              
+                Console.WriteLine(page);
+            }
+
+        }
+    }
+}
